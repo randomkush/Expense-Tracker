098d890cff58a18ced633b80c23e61150aa3fa4a
Kushagra Sharma <skushagra2003@gmail.com>
2021-05-22 20:17:01 +0530

    git op but sometimes it sucks

--- controllers/authenticate-controller.js
+++ controllers/authenticate-controller.js
@@ -52,4 +52,76 @@ module.exports.authenticate=function(req,res){
             })
       }
     });
+    // Pie Chart Shit
+    connection.query('SELECT * FROM transaction WHERE category = "Food" and email = ?',[email], function(error, results, fields){
+      if(error) {
+         console.log(error);
+      }
+      else {
+          app.get('/food', function(req, res) {
+              res.json(results);
+            })
+      }
+    });
+    connection.query('SELECT * FROM transaction WHERE category = "Health" and email = ?',[email], function(error, results, fields){
+      if(error) {
+         console.log(error);
+      }
+      else {
+          app.get('/health', function(req, res) {
+              res.json(results);
+            })
+      }
+    });
+    connection.query('SELECT * FROM transaction WHERE category = "Essentials" and email = ?',[email], function(error, results, fields){
+      if(error) {
+         console.log(error);
+      }
+      else {
+          app.get('/essen', function(req, res) {
+              res.json(results);
+            })
+      }
+    });
+    connection.query('SELECT * FROM transaction WHERE category = "Transportation" and email = ?',[email], function(error, results, fields){
+      if(error) {
+         console.log(error);
+      }
+      else {
+          app.get('/transport', function(req, res) {
+              res.json(results);
+            })
+      }
+    });
+    connection.query('SELECT * FROM transaction WHERE category = "Education" and email = ?',[email], function(error, results, fields){
+      if(error) {
+         console.log(error);
+      }
+      else {
+          app.get('/edu', function(req, res) {
+              res.json(results);
+            })
+      }
+    });
+    connection.query('SELECT * FROM transaction WHERE category = "Household" and email = ?',[email], function(error, results, fields){
+      if(error) {
+         console.log(error);
+      }
+      else {
+          app.get('/hhold', function(req, res) {
+              res.json(results);
+            })
+      }
+    });
+    connection.query('SELECT * FROM transaction WHERE category = "Others" and email = ?',[email], function(error, results, fields){
+      if(error) {
+         console.log(error);
+      }
+      else {
+          app.get('/others', function(req, res) {
+              res.json(results);
+            })
+      }
+    });
+
 }
